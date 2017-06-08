a32b1924570481fca540c263abd0e691a61a6356
anasajic <anaancisajic@gmail.com>
2017-06-08 12:31:22 +0200

    IT255-DZ12

--- it255-dz11/src/app/registracija/registracija.component.ts
+++ it255-dz11/src/app/registracija/registracija.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/Rx';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'registracija',
@@ -7,4 +11,47 @@ import {Component} from '@angular/core';
 
 })
 export class RegistracijaComponent {
+  http: Http;
+  router: Router;
+  data: string;
+
+  registerForma = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    ime: new FormControl(),
+    prezime: new FormControl(),
+    email: new FormControl()
+  });
+  constructor(http: Http, router: Router) {
+    this.http = http;
+    this.router = router;
+
+    if (localStorage.getItem('token') != null) {
+      this.router.navigate(['']);
+    }
+
+  }
+  registracija(): void {
+    this.data = 'username=' + this.registerForma.value.username + '&password=' + this.registerForma.value.password + '&ime=' +
+        this.registerForma.value.ime + '&prezime=' + this.registerForma.value.prezime + '&email=' + this.registerForma.value.email;
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this.http.post('http://localhost/it255-dz11/src/php/registracija.php', this.data, {headers: headers})
+        .map(res => res)
+        .subscribe(data => {
+              console.log(data);
+              let obj = JSON.parse(data['_body']);
+              localStorage.setItem('token', obj.token);
+              alert('Uspesna registracija!');
+              this.router.navigate(['']);
+            },
+            err => {
+              alert('Greska');
+              let obj = JSON.parse(err._body);
+              let element  = <HTMLElement> document.getElementsByClassName('alert')[0];
+              element.style.display = 'block';
+              element.innerHTML = obj.error.split('\\r\\n').join('<br/>').split('\"').join('');
+            }
+        );
+  }
 }

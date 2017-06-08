56de1f4919d134b660ffaac0c9cca17c3915a9ed
anasajic <anaancisajic@gmail.com>
2017-06-08 12:33:04 +0200

    IT255-DZ12

--- it255-dz11/src/app/header/header.component.ts
+++ it255-dz11/src/app/header/header.component.ts
@@ -1,8 +1,40 @@
 import {Component} from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'myheader',
-  templateUrl: './header.component.html',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-}
+
+  router: Router;
+  isAuth: String;
+  currentUrl: String;
+
+  constructor(router: Router) {
+    this.router = router;
+    this.currentUrl = '';
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (localStorage.getItem('token') !== null) {
+        this.isAuth = 'yes';
+      }else {
+        this.isAuth = 'no';
+      }
+    });
+  }
+  logout(): void {
+    alert('Uspesan logout');
+    localStorage.removeItem('token');
+    this.router.navigate(['']);
+    if (localStorage.getItem('token') !== null) {
+      this.isAuth = 'yes';
+    }else{
+      this.isAuth = 'no';
+    }
+  }
+  }
+
